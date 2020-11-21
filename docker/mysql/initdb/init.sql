22b02cf9425abcdc0e8127e01cf5e66d1eb28b88
Evgeniy Barinov <z.barinov@gmail.com>
2020-11-21 13:28:37 +0300

    AverageNumberOfPostsPerUserPerMonth: pre calculated

--- docker/mysql/initdb/init.sql
+++ docker/mysql/initdb/init.sql
@@ -1,20 +1,33 @@
-CREATE TABLE `posts`
+START TRANSACTION;
+
+CREATE TABLE IF NOT EXISTS `posts`
 (
-    `id`             varchar(26)                                           NOT NULL,
-    `from_name`      varchar(200)                                          NOT NULL,
-    `from_id`        varchar(200)                                          NOT NULL,
-    `message`        text                                                  NOT NULL,
-    `message_length` int                                                   NOT NULL,
-    `type`           varchar(20)                                           NOT NULL,
-    `created_time`   datetime                                              NOT NULL,
-    `month`          char(7) CHARACTER SET utf8 COLLATE utf8_general_ci    NOT NULL,
-    `week`           tinyint UNSIGNED                                      NOT NULL
+    `id`             varchar(26)                                        NOT NULL,
+    `from_name`      varchar(200)                                       NOT NULL,
+    `from_id`        varchar(200)                                       NOT NULL,
+    `message`        text                                               NOT NULL,
+    `message_length` int                                                NOT NULL,
+    `type`           varchar(20)                                        NOT NULL,
+    `created_time`   datetime                                           NOT NULL,
+    `month`          char(7) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+    `week`           tinyint UNSIGNED                                   NOT NULL
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
 ALTER TABLE `posts`
     ADD PRIMARY KEY (`id`),
-    ADD KEY `idx_month_from_name` (`month`, `from_name`) USING BTREE,
     ADD KEY `idx_month_message_length` (`month`, `message_length`) USING BTREE,
     ADD KEY `idx_week` (`week`) USING BTREE;
+
+CREATE TABLE IF NOT EXISTS `total_posts_per_user_per_month`
+(
+    `total_posts` int                                                     NOT NULL,
+    `month`       char(7) CHARACTER SET utf8 COLLATE utf8_general_ci      NOT NULL,
+    `from_name`   varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+ALTER TABLE `total_posts_per_user_per_month`
+    ADD UNIQUE KEY `idx_month_from_name` (`month`,`from_name`);
+
 COMMIT;
--- src/Metric/MySql/AverageNumberOfPostsPerUserPerMonth.php
+++ src/Metric/MySql/AverageNumberOfPostsPerUserPerMonth.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 namespace Barya\Stats\Metric\MySql;
 
 use \Barya\Stats\Metric\MetricInterface;
+use Barya\Stats\Metric\CalculatedMetricInterface;
 
-class AverageNumberOfPostsPerUserPerMonth extends Metric implements MetricInterface
+class AverageNumberOfPostsPerUserPerMonth extends Metric implements MetricInterface, CalculatedMetricInterface
 {
     /**
      * @inheritDoc
@@ -13,14 +14,21 @@ class AverageNumberOfPostsPerUserPerMonth extends Metric implements MetricInterf
     public function data() {
         $query = <<<QUERY
 SELECT ROUND(AVG(s1.total_posts), 2) AS avg_monthly, s1.from_name
-FROM (
-    SELECT COUNT(id) as total_posts, month, from_name
-    FROM posts GROUP BY month, from_name
-) AS s1 
+FROM total_posts_per_user_per_month AS s1 
 GROUP BY s1.from_name
 QUERY;
 
         return $this->db->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function calculate(): void {
+        $query = <<<QUERY
+INSERT INTO total_posts_per_user_per_month (
+    SELECT COUNT(id) as total_posts, month, from_name FROM posts GROUP BY month, from_name
+)
+QUERY;
+
+        $this->db->query($query)->execute();
     }
 
     public function name(): string {
